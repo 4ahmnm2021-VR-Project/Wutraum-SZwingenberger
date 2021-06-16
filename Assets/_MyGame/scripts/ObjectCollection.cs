ba9894419f52d174426009c257e1830d81714443
unknown <stefanie.zwingenberger03@htl-salzburg.ac.at>
2021-06-16 13:03:02 +0200

    destruct sccript

---
+++ Assets/_MyGame/scripts/ObjectCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollection : MonoBehaviour
+{
+    public List<GameObject> SmahedObjects = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_MyGame/scripts/Smash.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smash : MonoBehaviour
+{
+    private ObjectCollection objects;
+    private GameObject smashOBJ;
+    void Start()
+    {
+        objects = GameObject.Find("ObjectCollection").GetComponent<ObjectCollection>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+        if(collision.relativeVelocity.magnitude > 4f) {
+            foreach(GameObject prefap in objects.SmahedObjects) {
+                if(prefap.name == this.gameObject.name + "Destruct") {
+                    smashOBJ = prefap;
+                }
+            }
+            var spawn =  Instantiate(smashOBJ, transform.position, Quaternion.identity);
+            spawn.transform.position = this.gameObject.transform.position;
+            spawn.transform.rotation = this.gameObject.transform.rotation;
+
+            
+            foreach(Transform split in spawn.transform) {
+                var directionX = Random.Range(-8f, 8f);
+                var directionY = Random.Range(-4f, 4f);
+                Debug.Log("add velocity");
+                // split.GetComponent<Rigidbody>().AddForce(spawn.transform.up * -1 * 2000f);
+                split.GetComponent<Rigidbody>().AddForce(new Vector3(directionX, directionY) * 50f);
+            }
+            
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+    void Update() {
+
+    }
+}
